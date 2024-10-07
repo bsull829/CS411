845c9073ab0ba09139f05fe086072e9137d5b245
bsull829 <brian.sull.0803@gmail.com>
2024-10-07 13:46:06 -0400

    Committing extra changes made

--- HW3 Design/wildlife_tracker/migration_tracking/migration.py
+++ HW3 Design/wildlife_tracker/migration_tracking/migration.py
@@ -4,11 +4,15 @@ class Migration:
 
     def __init__(self,
                 migration_id: int,
-                status: str = "Scheduled") -> None:
+                status: str = "Scheduled",
+                current_date: str,
+                current_location: str) -> None:
         
         self.migration_id = migration_id
         self.status = status
         self.species = species
+        self.current_date = current_date
+        self.current_location = current_location
 
     def get_migration_details(self) -> dict[str, Any]:
        pass
