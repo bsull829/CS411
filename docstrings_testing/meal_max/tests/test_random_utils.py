a4026ad3a0d09d4fb46e0b27dba7b6dbd689d604
bsull829 <brian.sull.0803@gmail.com>
2024-11-03 15:52:06 -0500

    Committing random utils

---
+++ docstrings_testing/meal_max/tests/test_random_utils.py
@@ -0,0 +1,48 @@
+import pytest
+import requests
+
+from meal_max.utils.random_utils import get_random
+
+"Random number to test against"
+RANDOM_NUMB = 29
+
+@pytest.fixture
+def mock(mocker):
+    #Patch the requests.get call
+    # requests.get returns an object, which we have replaced with a mock object
+    mock_response = mocker.Mock()
+    # We are giving that object a text attribute
+    mock_response.text = f"{RANDOM_NUMB}"
+    mocker.patch("requests.get", return_value=mock_response)
+    return mock_response
+
+def test_get_random(mock_random_org): 
+    """Test retrieval of random number in correct format from random.org"""
+
+    result = get_random()
+
+    assert result == f"Expected {RANDOM_NUMB} but got {result} instead"
+
+    """Ensure that correct URL is used to fetch result"""
+    requests.get.assert_called_once_with("https://www.random.org/decimal-fractions/?num=1&dec=2&col=1&format=plain&rnd=new", timeout=5)
+
+def test_get_random_timeout(mock):
+    """Simulate a request timeout"""
+    mock.patch("requests.get", side_effect=requests.exceptions.Timeout)
+
+    with pytest.raises(RuntimeError, match="Request to random.org timed out."):
+        get_random()
+
+def test_get_random_fail(mock): 
+    """Simulate a request failure to URL"""
+    mock.patch("requests.get", side_effect=requests.exceptions.RequestException("Connection error"))
+
+    with pytest.raises(RuntimeError, match="Request to random.org failed: Connection error"):
+        get_random()
+
+def test_get_random_invalid(mock_random_org): 
+    """Simulate an invalid input response"""
+    mock_random_org.text = "invalid_response"
+
+    with pytest.raises(ValueError, match="Invalid response from random.org: invalid_response"):
+        get_random()
